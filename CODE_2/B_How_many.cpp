e63504894c2d83fb0365620991de2df0e6b5debc
rifatbroh <163645638+rifatbroh@users.noreply.github.com>
2024-03-17 16:36:09 +0600

    Add files via upload

---
+++ CODE_2/B_How_many.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main ()
+{
+    int s, t;
+    cin >> s >> t;
+    int count = 0;
+
+    for (int a=0; a<=s; a++)
+    {
+        for (int b=0; b<=s; b++)
+        {
+            for (int c=0; c<=s; c++)
+            {
+                if (s >= (a+b+c) && t>= (a*b*c))
+                {
+                    count++;
+                }
+            }
+        }
+    }
+    cout << count << endl;
+    return 0;
+}
