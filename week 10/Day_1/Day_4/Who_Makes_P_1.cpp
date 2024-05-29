b98def58b5478a359dbb3cc88a094d8fcd9b546e
rifatbroh <163645638+rifatbroh@users.noreply.github.com>
2024-05-29 22:29:05 +0600

    Add files via upload
    day 4

---
+++ week 10/Day_1/Day_4/Who_Makes_P_1.cpp
@@ -0,0 +1,20 @@
+/*
+    MD Rifat 
+    
+    country Bangladesh
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int x, y;
+    cin >> x >> y;
+    
+    if (y >= x)
+        cout << "Tyro" << endl;
+    else
+        cout << "Dom" << endl;
+    
+    return 0;
+}
