6e79119066f4e679621001c6b99e49787ca778d7
rifatbroh <163645638+rifatbroh@users.noreply.github.com>
2024-06-01 20:28:13 +0600

    Add files via upload
    day 1

---
+++ week 11/Day_1/Sum_of_Modes.cpp
@@ -0,0 +1,56 @@
+/*
+    MD Rifat Hossain
+
+    Institute:- BAIUST
+    Country:- BANGLADESH
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define OLD_MAXTAN                 \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define ll long long
+#define ld long double
+#define yes "YES"
+#define no "no"
+#define PI 3.1415926535897932384626433832795
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+void solve(){
+    ll n;
+    cin>>n;
+    string s;
+    cin>>s;
+    map<ll, ll> mp;
+    ll sum = 0;
+    ll cnt = 0;
+    mp[0] = 1;
+    for(ll i=0;i<n;i++){
+        sum += (s[i]=='0') ? 1 : -1;
+        if(mp.find(sum) != mp.end()){
+            cnt += mp[sum];
+        }
+        mp[sum]++;
+    }
+    ll ans = n*(n+1)/2;
+    cout<<ans+cnt;
+}
+int main()
+{
+    OLD_MAXTAN
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+        cout<<"\n";
+    }
+    return 0;
+}
