41ee231c4259319c0d9f07bf3c7ac029fcd93990
rifatbroh <163645638+rifatbroh@users.noreply.github.com>
2024-06-09 22:35:45 +0600

    Add files via upload
    day 2

---
+++ week 12/Day_1/Day_2/C_Birthday.cpp
@@ -0,0 +1,28 @@
+/*
+    MD Rifat 
+    
+    country Bangladesh
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+int a[105];
+ 
+int main(){
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++){
+		cin >> a[i];
+	}	
+	sort(a,a+n);
+	for (int i = 0; i < n; i += 2){
+		cout << a[i] << " ";
+	}
+	if(n % 2) n-=2;
+	else n--;
+	for (int i = n; i > 0 ; i-=2){
+		cout << a[i] << " ";
+	}
+	cout << endl;
+	return 0;
+} 
