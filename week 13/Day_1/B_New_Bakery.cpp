58ece0bfe5d3c35a63a203c959aeebfa3b5fc53c
rifatbroh <163645638+rifatbroh@users.noreply.github.com>
2024-06-23 20:43:16 +0600

    Add files via upload
    day 1

---
+++ week 13/Day_1/B_New_Bakery.cpp
@@ -0,0 +1,50 @@
+/*
+    MD Rifat Hossain
+
+    Institute:- BAIUST
+    Country:- BANGLADESH
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define OLD_MAXTAN                 \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define ll long long
+#define ld long double
+#define yes "YES"
+#define no "no"
+#define PI 3.1415926535897932384626433832795
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+void solve()
+{
+    /* let's play */
+    ll n, a, b;
+    cin >> n >> a >> b;
+    if (b <= a) {
+        cout << n * a;
+    } else {
+        ll k = min(b - a + 1, n);
+        cout << (b - k + 1) * n + k * (k - 1) / 2 ;
+    }
+}
+
+int main()
+{
+    OLD_MAXTAN
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+        cout<<"\n";
+    }
+    return 0;
+}
