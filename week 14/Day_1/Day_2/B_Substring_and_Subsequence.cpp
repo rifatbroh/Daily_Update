e15d51a0daefe8025827ba7af4210acf312c7408
rifatbroh <163645638+rifatbroh@users.noreply.github.com>
2024-06-30 18:08:24 +0600

    Add files via upload
    day 2

---
+++ week 14/Day_1/Day_2/B_Substring_and_Subsequence.cpp
@@ -0,0 +1,59 @@
+/*
+    MD Rifat Hossain
+
+    Institute:- BAIUST
+    Country:- BANGLADESH
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define OLD_MAXTAN                 \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define ll long long
+#define ld long double
+#define yes "YES"
+#define no "no"
+#define PI 3.1415926535897932384626433832795
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+void solve()
+{
+    /*Lets play*/
+    string a,b;
+    cin>>a>>b;
+    int ans = a.size() + b.size();
+
+    for(int start=0;start<b.size();start++)
+    {
+        int p2 = start;
+        for(int p1=0;p1<a.size() && p2<b.size();p1++)
+        {
+            if(a[p1] == b[p2])
+            {
+                p2++;
+            }
+        }
+        int need =  a.size() + b.size() - (p2 - start);
+        ans = min(ans, need);
+    }
+    cout<<ans;
+}
+int main()
+{
+    OLD_MAXTAN
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+        cout<<"\n";
+    }
+    return 0;
+}
