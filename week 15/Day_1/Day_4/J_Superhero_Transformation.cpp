ef03394d1aacee3c5a583010af56b0cd976d01ab
rifatbroh <163645638+rifatbroh@users.noreply.github.com>
2024-07-09 21:04:00 +0600

    Add files via upload
    day 3

---
+++ week 15/Day_1/Day_4/J_Superhero_Transformation.cpp
@@ -0,0 +1,34 @@
+/*
+    MD Rifat
+    
+    country Bangladesh
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isVowel(char ch) {
+    return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u';
+}
+
+int main() {
+    string s1, s2;
+    cin >> s1 >> s2;
+
+    int len_s1 = s1.size(), len_s2 = s2.size();
+
+    if (len_s1 != len_s2) {
+        cout << "No" << endl;
+        return 0;
+    }
+
+    for (int i = 0; i < len_s1; i++) {
+        if (isVowel(s1[i]) != isVowel(s2[i])) {
+            cout << "No" << endl;
+            return 0;
+        }
+    }
+
+    cout << "Yes" << endl;
+    return 0;
+}
