4bd69a6d2691dbba44f63ecc28f3b447e1ef4207
rifatbroh <163645638+rifatbroh@users.noreply.github.com>
2024-08-20 17:21:28 +0600

    Add files via upload

---
+++ week 17/Day_3/C_Tiles_Comeback.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define OLD_MAXTAN                 \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define ll long long
+#define ld long double
+#define yes "YES"
+#define no "NO"
+#define PI 3.1415926535897932384626433832795
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+void solve()
+{
+    /*Lets play*/
+    int n, k;
+    cin >> n >> k;
+    vector<int> c(n);
+    for(int i = 0; i < n; i++) cin >> c[i];
+    int l = 0, r = 0, i = 0, j = n - 1;
+    int left = k, right = k;
+
+    if (c[0] == c[n - 1]) {
+        left = k / 2;
+        right = k - left;
+    }
+    for(; i < n && l < left; i++) {
+        if(c[i] == c[0]) l++;
+    }
+    for(; j >= 0 && r < right; j--) {
+        if(c[j] == c[n - 1]) r++;
+    }
+    if ((i - 1) < (j + 1))
+        cout << yes;
+    else
+        cout << no;
+}
+
+int main()
+{
+    OLD_MAXTAN
+    int t;
+    cin >> t;
+    while(t--){
+       solve();
+       cout << '\n';
+    }
+    return 0;
+}
