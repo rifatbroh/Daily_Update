5abed287417e454d15b4a26b1f88cc0a79198673
rifatbroh <163645638+rifatbroh@users.noreply.github.com>
2024-08-23 23:50:06 +0600

    Add files via upload

---
+++ week 16/Day_2/C_Increasing_Sequence_with_Fixed_OR.cpp
@@ -0,0 +1,66 @@
+/*
+    MD Rifat Hossain
+
+    Institute:- BAIUST
+    Country:- BANGLADESH
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define OLD_MAXTAN                 \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define ll long long
+#define ld long double
+#define yes "YES"
+#define no "no"
+#define PI 3.1415926535897932384626433832795
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+void solve()
+{
+    /*Lets play*/
+        int n;
+        cin >> n;
+        int cnt = 0;
+        vector<int> v;
+        vector<int> pos;
+        
+        for (int i = 0; i < 61; i++) {
+            if (n & (1LL << i)) {
+                cnt++;
+                pos.push_back((1LL << i));
+            }
+        }
+     
+        v.push_back(n);
+        for (auto bit : pos) {
+            if (n - bit > 0) {
+                v.push_back(n - bit);
+            }
+        }
+        reverse(v.begin(), v.end());
+
+        cout << v.size() << endl;
+        for (auto element : v)
+            cout << element << " ";
+
+}
+int main()
+{
+    OLD_MAXTAN
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+        cout<<"\n";
+    }
+    return 0;
+}
