24a7fb76e7c747f3472a8184e1e6157b57694e57
rifatbroh <163645638+rifatbroh@users.noreply.github.com>
2024-08-25 22:33:58 +0600

    Add files via upload

---
+++ week 18/Day_1/A_Turtle_and_Good_Strings.cpp
@@ -0,0 +1,70 @@
+/*
+    MD Rifat Hossain
+
+    Institute:- BAIUST
+    Country:- BANGLADESH
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define OLD_MAXTAN                 \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+#define ll long long
+#define ld long double
+#define yes cout << "YES"
+#define no cout << "NO"
+#define pb push_back
+#define vi vector<int>
+#define vp vector<pair<int, int>>
+#define mp map<int, int>
+#define vec(v) v.begin(), v.end()
+/* !    !   !   !   !   !   !   */
+#define PI 3.1415926535897932384626433832795
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+void solve()
+{
+    /* _THE^MAXTAN_*/
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+
+    // bool flag = false;
+    // for (int i=0; i<n-1; i++){
+    //     if (s[i] != s[i+1]){
+    //         flag = true;
+    //         break;
+    //     }
+    // }
+    // if (flag)
+    //     cout << "YES";
+    // else
+    //     cout << "NO";
+
+
+    if (s[0] == s[n-1])
+        no;
+    else
+        yes;
+}
+int main()
+{
+    OLD_MAXTAN
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+        cout<<"\n";
+    }
+    return 0;
+}
