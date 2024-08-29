e37011e0bee8e075334d8aafd8af30ef9a9978b2
rifatbroh <163645638+rifatbroh@users.noreply.github.com>
2024-08-29 17:49:51 +0600

    Add files via upload

---
+++ week 15/Day_2/Penalty_Shoot_out.cpp
@@ -0,0 +1,48 @@
+/*
+    MD Rifat Hossain
+
+    Institute:- BAIUST
+    Country:- BANGLADESH
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define OLD_MAXTAN                 \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define ll long long
+#define ld long double
+#define yes "YES"
+#define no "NO"
+#define PI 3.1415926535897932384626433832795
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+void solve()
+{
+    /* Lets GO */
+    int x, y;
+    cin >> x >> y;
+    int diff = x - y;
+    if (diff >= -2 && diff <= 1)
+        cout << yes<<endl;
+    else
+        cout << no << endl;
+}
+
+int main()
+{
+    OLD_MAXTAN
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
