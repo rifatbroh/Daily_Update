a86606eee0ff514178b4dd9dcfae5ea40cba0c70
rifatbroh <163645638+rifatbroh@users.noreply.github.com>
2024-09-07 21:13:14 +0600

    Add files via upload

---
+++ week 19/Day_4/B_New_Bakery.cpp
@@ -0,0 +1,56 @@
+/*
+    MD Rifat Hossain
+
+    Institute:- BAIUST
+    Country:- BANGLADESH
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define OLD_MAXTAN                 \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+#define ll long long
+#define ld long double
+#define yes cout << "YES"
+#define no cout << "NO"
+#define pb push_back
+#define vi vector<int>
+#define vp vector<pair<int, int>>
+#define mp map<int, int>
+#define vec(v) v.begin(), v.end()
+/* !    !   !   !   !   !   !   */
+#define PI 3.1415926535897932384626433832795
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+void solve()
+{
+    /* _THE^MAXTAN_*/
+    ll n, a, b;
+    cin >> n >> a >> b;
+    if (a >= b) {
+        cout << n * a;
+    } else {
+        ll k = min(b - a + 1, n);
+        cout << (b - k + 1) * n + k * (k - 1) / 2 ;
+    }
+}
+int main()
+{
+    OLD_MAXTAN
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+        cout<<"\n";
+    }
+    return 0;
+}
