de9864896534a30d5697b1fa61e94f7e9e5d4a32
rifatbroh <163645638+rifatbroh@users.noreply.github.com>
2024-09-07 21:12:36 +0600

    Add files via upload

---
+++ week 19/Day_4/A_Satisfying_Constraints.cpp
@@ -0,0 +1,71 @@
+/*
+    MD Rifat Hossain
+
+    Institute:- BAIUST
+    Country:- BANGLADESH
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define OLD_MAXTAN                 \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+#define ll long long
+#define ld long double
+#define yes cout << "YES"
+#define no cout << "NO"
+#define pb push_back
+#define vi vector<int>
+#define vp vector<pair<int, int>>
+#define mp map<int, int>
+#define vec(v) v.begin(), v.end()
+/* !    !   !   !   !   !   !   */
+#define PI 3.1415926535897932384626433832795
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+void solve()
+{
+    /* _THE^MAXTAN_*/
+    int q;
+    cin >> q;
+    int l = 0, r = 1e18;
+    int s = 0, ans;
+    vector<int> v;
+
+    while(q--){
+        int a, x;
+        cin >> a >> x;
+
+        if (a==1)
+            l = max(l, x);
+        else if (a==2)
+            r = min(r, x);
+        else
+            v.push_back(x);
+    }
+    for (auto i : v){
+        if (i >= l && i <= r)
+            s++;
+    }
+    ans = max(r-l+1-s, 0);
+    cout << ans;
+}
+int main()
+{
+    OLD_MAXTAN
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+        cout<<"\n";
+    }
+    return 0;
+}
