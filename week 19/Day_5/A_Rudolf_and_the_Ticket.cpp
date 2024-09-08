e3206b68f95586ed5bb7617da97a2132794f851b
rifatbroh <163645638+rifatbroh@users.noreply.github.com>
2024-09-08 14:11:04 +0600

    Add files via upload

---
+++ week 19/Day_5/A_Rudolf_and_the_Ticket.cpp
@@ -0,0 +1,63 @@
+/*
+    MD Rifat Hossain
+
+    Institute:- BAIUST
+    Country:- BANGLADESH
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define OLD_MAXTAN                 \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+#define ll long long
+#define ld long double
+#define yes cout << "YES"
+#define no cout << "NO"
+#define pb push_back
+#define vi vector<int>
+#define vp vector<pair<int, int>>
+#define mp map<int, int>
+#define vec(v) v.begin(), v.end()
+/* !    !   !   !   !   !   !   */
+#define PI 3.1415926535897932384626433832795
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+void solve()
+{
+    /* _THE^MAXTAN_*/
+    int n, m, k;
+    cin >> n >> m >> k;
+    vi a(n), b(m);
+    for (int i=0; i<n; i++) cin >> a[i];
+    for (int i=0; i<m; i++) cin >> b[i];
+
+    int ans = 0;
+    for (int i=0; i<n; i++){
+        for (int j=0; j<m; j++){
+            if (a[i] + b[j] <= k){
+                ans++;
+            }
+        }
+    }
+    cout << ans ;
+}
+int main()
+{
+    OLD_MAXTAN
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+        cout<<"\n";
+    }
+    return 0;
+}
