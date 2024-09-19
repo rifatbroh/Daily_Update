09024b44f9edb1f726cc5da696a1fa598aa5fc8a
rifatbroh <163645638+rifatbroh@users.noreply.github.com>
2024-09-19 20:58:13 +0600

    Add files via upload

---
+++ week 20/Day_3/A_Jagged_Swaps.cpp
@@ -0,0 +1,63 @@
+/*
+    MD Rifat Hossain
+
+    Institute:- BAIUST
+    Country:- BANGLADESH
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define OLD_MAXTAN                 \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+#define ll long long
+#define ld long double
+#define yes cout << "YES"
+#define no cout << "NO"
+#define pb push_back
+#define vi vector<int>
+#define vp vector<pair<int, int>>
+#define mp map<int, int>
+#define vec(v) v.begin(), v.end()
+/* !    !   !   !   !   !   !   */
+#define PI 3.1415926535897932384626433832795
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+void solve()
+{
+    /* _THE^MAXTAN_*/
+    int n;
+    cin >> n;
+    vi v(n);
+    for (int i=0; i<n; i++) cin >> v[i];
+
+    // for (int i=1; i<n-1; i++){
+    //     if (v[i-1] < v[i] && v[i] > v[i+1]){
+    //         swap(v[i], v[i+1]);
+    //     }
+    // }
+    
+    if (v[0] == 1)
+        yes;
+    else
+        no;
+}
+int main()
+{
+    OLD_MAXTAN
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+        cout<<"\n";
+    }
+    return 0;
+}
