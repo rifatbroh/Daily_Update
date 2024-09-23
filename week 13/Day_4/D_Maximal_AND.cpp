d0adda750d2d8ff30b88484e89c2310ac6798be7
rifatbroh <163645638+rifatbroh@users.noreply.github.com>
2024-09-23 20:37:19 +0600

    Add files via upload

---
+++ week 13/Day_4/D_Maximal_AND.cpp
@@ -0,0 +1,58 @@
+/*
+    MD Rifat Hossain
+
+    Institute:- BAIUST
+    Country:- BANGLADESH
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define OLD_MAXTAN                 \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define ll long long
+#define ld long double
+#define yes "YES"
+#define no "no"
+#define PI 3.1415926535897932384626433832795
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+void solve()
+{
+    /*Lets play*/
+    int n, k; cin >> n >> k;
+    vector<int> cnt (31, 0), a(n);
+    for(int i = 0;i < n; ++i) {
+        cin >> a[i];
+        for(int j = 30; j >= 0; j--) {
+            if(a[i] & (1 << j)) cnt[j]++;
+        }
+    }
+    ll res = 0;
+    for(int i = 30; i >= 0; i--) {
+        int cal = n - cnt[i];
+        if(cal <= k) {
+            k -= cal;
+            res += (1 << i);
+        }
+    }
+    cout << res ;
+}
+int main()
+{
+    OLD_MAXTAN
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+        cout<<"\n";
+    }
+    return 0;
+}
