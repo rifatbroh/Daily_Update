d250b3f4e7fbf8f3dff495a06ab45d8cb0f09c57
rifatbroh <163645638+rifatbroh@users.noreply.github.com>
2024-09-24 19:05:28 +0600

    Add files via upload

---
+++ week 20/Day_5/A_Goals_of_Victory.cpp
@@ -0,0 +1,59 @@
+/*
+    MD Rifat Hossain
+
+    Institute:- BAIUST
+    Country:- BANGLADESH
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define OLD_MAXTAN                 \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+#define ll long long
+#define ld long double
+#define yes cout << "YES"
+#define no cout << "NO"
+#define pb push_back
+#define vi vector<int>
+#define vp vector<pair<int, int>>
+#define mp map<int, int>
+#define vec(v) v.begin(), v.end()
+/* !    !   !   !   !   !   !   */
+#define PI 3.1415926535897932384626433832795
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+void solve()
+{
+    /* _THE^MAXTAN_*/
+    int n;
+    cin >> n;
+    vi v(n-1);
+    for (int i=0; i<n-1; i++) cin >> v[i];
+
+    int sum = 0;
+    for (int i=0; i<n-1; i++){
+        sum += v[i];
+    }
+    cout << -sum ;
+
+}
+int main()
+{
+    OLD_MAXTAN
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+        cout<<"\n";
+    }
+    return 0;
+}
