9d302df6da25647570a9bf193fcf2f4854db9497
rifatbroh <163645638+rifatbroh@users.noreply.github.com>
2024-10-01 19:22:16 +0600

    Add files via upload

---
+++ week 12/Day_4/B_Maximum_Multiple_Sum.cpp
@@ -0,0 +1,57 @@
+/*
+    MD Rifat Hossain
+
+    Institute:- BAIUST
+    Country:- BANGLADESH
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define OLD_MAXTAN                 \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define ll long long
+#define ld long double
+#define yes "YES"
+#define no "no"
+#define PI 3.1415926535897932384626433832795
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+void solve()
+{
+    /*Lets play*/
+        int n;
+        cin >> n;
+    
+        int calculate = 2, mx = 0;
+        for (int i = 2; i<=n; i++)
+        {
+            int k = n / i;
+            int sum = i * (k * (k + 1)) / 2;
+            
+            if (sum > mx)
+            {
+                mx= sum;
+                calculate = i;
+            }
+        } 
+        cout << calculate ;
+}
+int main()
+{
+    OLD_MAXTAN
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+        cout<<"\n";
+    }
+    return 0;
+}
