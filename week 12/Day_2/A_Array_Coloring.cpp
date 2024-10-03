18a8a75e07f241e5180ec44c99ddef19435d41b4
rifatbroh <163645638+rifatbroh@users.noreply.github.com>
2024-10-03 20:36:38 +0600

    Add files via upload

---
+++ week 12/Day_2/A_Array_Coloring.cpp
@@ -0,0 +1,66 @@
+/*
+    MD Rifat Hossain
+
+    Institute:- BAIUST
+    Country:- BANGLADESH
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define OLD_MAXTAN                 \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+#define ll long long
+#define ld long double
+#define yes cout << "YES"
+#define no cout << "NO"
+#define pb push_back
+#define vi vector<int>
+#define vp vector<pair<int, int>>
+#define mp map<int, int>
+#define vec(v) v.begin(), v.end()
+/* !    !   !   !   !   !   !   */
+#define PI 3.1415926535897932384626433832795
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+void solve()
+{
+    /* _THE^MAXTAN_*/
+    int n;
+    cin >> n;
+    vi v(n);
+    for (int i=0; i<n; i++) cin >> v[i];
+
+    int even = 0, odd = 0;
+   
+    for (int i=0; i<n; i++){
+        if (v[i]%2 == 0)
+            even += v[i];
+        else
+            odd += v[i];
+    }
+    if (even%2 == 0 && odd%2 == 0)
+        cout << "YES";
+    else
+        cout << "NO";
+    // cout << even << " " << odd;
+}
+int main()
+{
+    OLD_MAXTAN
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+        cout<<"\n";
+    }
+    return 0;
+}
