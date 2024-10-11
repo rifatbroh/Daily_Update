fd1816092f3bc4e2aa77f4fc2585e0ad4e9fe79a
rifatbroh <163645638+rifatbroh@users.noreply.github.com>
2024-10-12 02:41:43 +0600

    Add files via upload

---
+++ week 11/Day_1/A_Twin_Permutations.cpp
@@ -0,0 +1,67 @@
+/*
+    MD Rifat Hossain
+
+    Institute:- BAIUST
+    Country:- BANGLADESH
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define OLD_MAXTAN                 \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+#define ll long long
+#define ld long double
+#define yes cout << "YES"
+#define no cout << "NO"
+#define pb push_back
+#define vi vector<int>
+#define vp vector<pair<int, int>>
+#define mp map<int, int>
+#define vec(v) v.begin(), v.end()
+/* !    !   !   !   !   !   !   */
+#define PI 3.1415926535897932384626433832795
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+void solve()
+{
+    /* _THE^MAXTAN_*/
+    int n;
+    cin >> n;
+    vi a(n);
+    for (int i=0; i<n; i++) cin >> a[i];
+    // vi b = a;
+
+    // sort(b.begin(), b.end());
+    // // for (int i=0; i<n; i++){
+    // //     if (a[i]+b[i] <= a[i+1]+b[i+1]){
+    // //         cout << b[i] <<" ";
+    // //     }
+    // // }
+
+    // for (int i=0; i<n; i++){
+    //     cout << b[i] << " ";
+    // }
+    for (int i=0; i<n; i++){
+        cout << n - a[i] + 1 << " ";
+    }
+}
+int main()
+{
+    OLD_MAXTAN
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+        cout<<"\n";
+    }
+    return 0;
+}
