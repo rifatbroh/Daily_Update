dad4219d3e2b6ecfdb58e907251804c22120d3c2
rifatbroh <163645638+rifatbroh@users.noreply.github.com>
2024-10-13 15:31:15 +0600

    Add files via upload

---
+++ week 11/Day_1/A_Walking_Master.cpp
@@ -0,0 +1,61 @@
+/*
+    MD Rifat Hossain
+
+    Institute:- BAIUST
+    Country:- BANGLADESH
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define OLD_MAXTAN                 \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+#define ll long long
+#define ld long double
+#define yes cout << "YES"
+#define no cout << "NO"
+#define pb push_back
+#define vi vector<int>
+#define vp vector<pair<int, int>>
+#define mp map<int, int>
+#define vec(v) v.begin(), v.end()
+/* !    !   !   !   !   !   !   */
+#define PI 3.1415926535897932384626433832795
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+void solve()
+{
+    /* _THE^MAXTAN_*/
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+
+    // int move = 0;
+    // while(a != c && b != d){
+    //     move += ((a+1)+(b+1) || (a-1)+b);
+    // }
+    // cout << move;
+
+    if (b <=d && c <= a+d-b)
+        cout << (d-b)+(a+d-b-c);
+    else
+        cout << "-1";
+}
+int main()
+{
+    OLD_MAXTAN
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+        cout<<"\n";
+    }
+    return 0;
+}
