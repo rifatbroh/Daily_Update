030bfc33052b640e8c67fb945d37e18c2f7e3ae3
rifatbroh <163645638+rifatbroh@users.noreply.github.com>
2024-10-15 23:52:08 +0600

    Add files via upload

---
+++ week 11/Day_2/A_One_and_Two.cpp
@@ -0,0 +1,70 @@
+/*
+    MD Rifat Hossain
+
+    Institute:- BAIUST
+    Country:- BANGLADESH
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define OLD_MAXTAN                 \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+#define ll long long
+#define ld long double
+#define yes cout << "YES"
+#define no cout << "NO"
+#define pb push_back
+#define vi vector<int>
+#define vp vector<pair<int, int>>
+#define mp map<int, int>
+#define vec(v) v.begin(), v.end()
+/* !    !   !   !   !   !   !   */
+#define PI 3.1415926535897932384626433832795
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+void solve()
+{
+    /* _THE^MAXTAN_*/
+    int n;
+    cin >> n;
+    vi v(n);
+    for (int i=0; i<n; i++) cin >> v[i];
+
+    for (int i=0; i<n; i++)
+    {
+        int left = 0, right = 0;
+        for (int j=0; j<=i; j++){
+            if (v[j] == 1) continue;
+            left += v[j];
+        }
+        for (int j=i+1; j<n; j++){
+            if (v[j] == 1) continue;
+            right += v[j];
+        }
+        if (left == right){
+            cout << i+1;
+            return;
+        }
+    }
+    cout << "-1";
+}
+int main()
+{
+    OLD_MAXTAN
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+        cout<<"\n";
+    }
+    return 0;
+}
