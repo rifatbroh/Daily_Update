322614c5e058d85a07ab3a0e8fb7711e0ecbb66c
rifatbroh <163645638+rifatbroh@users.noreply.github.com>
2024-10-30 22:52:35 +0600

    Add files via upload

---
+++ week 10/Day_1/A_Extremely_Round.cpp
@@ -0,0 +1,65 @@
+/*
+    MD Rifat Hossain
+
+    Institute:- BAIUST
+    Country:- BANGLADESH
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define OLD_MAXTAN                 \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+#define ll long long
+#define ld long double
+#define yes cout << "YES"
+#define no cout << "NO"
+#define pb push_back
+#define vi vector<int>
+#define vp vector<pair<int, int>>
+#define mp map<int, int>
+#define vec(v) v.begin(), v.end()
+/* !    !   !   !   !   !   !   */
+#define PI 3.1415926535897932384626433832795
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+void solve()
+{
+    /* _THE^MAXTAN_*/
+    int n;
+    cin >> n;
+    int res = 0;
+    int x = 1, y = 1, z = 1;
+    
+    while(x <=n){
+        res++;
+        x += y;
+        if (z == 9){
+            z = 1;
+            y *= 10LL;
+            x = y;
+            continue;
+        }
+        z++;
+    }
+    cout << res ;
+}
+int main()
+{
+    OLD_MAXTAN
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+        cout<<"\n";
+    }
+    return 0;
+}
