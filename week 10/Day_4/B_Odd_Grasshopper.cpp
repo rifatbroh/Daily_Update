b7fbe3197353a5418ff747e5cf95eaceb444f119
rifatbroh <163645638+rifatbroh@users.noreply.github.com>
2024-11-12 18:28:37 +0600

    Add files via upload

---
+++ week 10/Day_4/B_Odd_Grasshopper.cpp
@@ -0,0 +1,71 @@
+/*
+    MD Rifat Hossain
+
+    Institute:- BAIUST
+    Country:- BANGLADESH
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define OLD_MAXTAN                 \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+#define ll long long
+#define ld long double
+#define yes cout << "YES"
+#define no cout << "NO"
+#define pb push_back
+#define vi vector<int>
+#define vp vector<pair<int, int>>
+#define mp map<int, int>
+#define vec(v) v.begin(), v.end()
+/* !    !   !   !   !   !   !   */
+#define PI 3.1415926535897932384626433832795
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+void solve()
+{
+    /* _THE^MAXTAN_*/
+    ll s, n, d;
+    cin >> s >> n;
+ 
+    switch (n % 4)
+    {
+    case 0:
+        d = 0;
+        break;
+    case 1:
+        d = -n;
+        break;
+    case 2:
+        d = 1;
+        break;
+    case 3:
+        d = n + 1;
+        break;
+    }
+ 
+    if (s % 2 == 0)
+        cout << s + d;
+    else
+        cout << s - d;
+}
+int main()
+{
+    OLD_MAXTAN
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+        cout<<"\n";
+    }
+    return 0;
+}
