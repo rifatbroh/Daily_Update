893c26ba21418288a993e4f5307bb7140c13191f
rifatbroh <163645638+rifatbroh@users.noreply.github.com>
2024-11-15 19:03:50 +0600

    Add files via upload

---
+++ week 10/Day_5/A_Jellyfish_and_Undertale.cpp
@@ -0,0 +1,58 @@
+/*
+    MD Rifat Hossain
+
+    Institute:- BAIUST
+    Country:- BANGLADESH
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define OLD_MAXTAN                 \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+#define ll long long
+#define ld long double
+#define yes cout << "YES"
+#define no cout << "NO"
+#define pb push_back
+#define vi vector<int>
+#define vp vector<pair<int, int>>
+#define mp map<int, int>
+#define vec(v) v.begin(), v.end()
+/* !    !   !   !   !   !   !   */
+#define PI 3.1415926535897932384626433832795
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+void solve()
+{
+    /* _THE^MAXTAN_*/
+    int a, b, n;
+    cin >> a >> b >> n;
+    vi v(n);
+    for (int i=0; i<n; i++) cin >> v[i];
+
+    for (int i=0; i<n; i++){
+        b += min(a-1, v[i]);
+    }
+
+    cout << b;
+}
+int main()
+{
+    OLD_MAXTAN
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+        cout<<"\n";
+    }
+    return 0;
+}
