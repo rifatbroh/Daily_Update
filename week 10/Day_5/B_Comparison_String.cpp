01eb76dc28e355458ac0208a35813e75bb2f6157
rifatbroh <163645638+rifatbroh@users.noreply.github.com>
2024-11-15 19:04:23 +0600

    Add files via upload

---
+++ week 10/Day_5/B_Comparison_String.cpp
@@ -0,0 +1,67 @@
+/*
+    MD Rifat Hossain
+
+    Institute:- BAIUST
+    Country:- BANGLADESH
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define OLD_MAXTAN                 \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+#define ll long long
+#define ld long double
+#define yes cout << "YES"
+#define no cout << "NO"
+#define pb push_back
+#define vi vector<int>
+#define vp vector<pair<int, int>>
+#define mp map<int, int>
+#define vec(v) v.begin(), v.end()
+/* !    !   !   !   !   !   !   */
+#define PI 3.1415926535897932384626433832795
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+void solve()
+{
+    /* _THE^MAXTAN_*/
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+
+    int mx = 0;
+    for (int i=0; i<s.size(); i++){
+       int cnt = 0;
+        for (int j=i; j<n; j++){
+            if (s[i] == s[j]){
+                cnt++;
+            }
+            else{
+                break;
+            }
+       }
+       mx = max(mx, cnt);
+    }
+    cout << mx+1 ;
+}
+int main()
+{
+    OLD_MAXTAN
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+        cout<<"\n";
+    }
+    return 0;
+}
