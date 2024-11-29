1015bd4efe950696e4ca82d6aaeb01f8a4fc80e2
rifatbroh <163645638+rifatbroh@users.noreply.github.com>
2024-11-29 20:15:13 +0600

    Add files via upload

---
+++ weak 8/Day_1/B_Permutation_Swap.cpp
@@ -0,0 +1,71 @@
+/*
+    MD Rifat Hossain
+
+    Institute:- BAIUST
+    Country:- BANGLADESH
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define OLD_MAXTAN                 \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+#define ll long long
+#define ld long double
+#define yes cout << "YES"
+#define no cout << "NO"
+#define pb push_back
+#define vi vector<int>
+#define vp vector<pair<int, int>>
+#define mp map<int, int>
+#define vec(v) v.begin(), v.end()
+/* !    !   !   !   !   !   !   */
+#define PI 3.1415926535897932384626433832795
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+void solve()
+{
+    /* _THE^MAXTAN_*/
+    int n;
+    cin >> n;
+    vi v(n+1);
+    for (int i=1; i<=n; i++) cin >> v[i];
+
+    // here i build logic
+    // int cnt = 0, mx = 0;
+    // for (int i=0; i<n; i++){
+    //     for (int j=0; j<n; j++){
+    //         if (abs(v[i] - v[i+1])){
+    //             cnt++;
+    //         }
+    //         else{
+    //             mx = max(mx, cnt);
+    //         }
+    //     }
+    // }
+    
+    int ans = 0;
+    for (int i=1; i<=n; i++){
+        ans = __gcd(ans, abs(i - v[i]));
+    }
+    cout << ans ;
+}
+int main()
+{
+    OLD_MAXTAN
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+        cout<<"\n";
+    }
+    return 0;
+}
