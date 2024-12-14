a2be2c0f8aba73a36f87d10061d3192d2521ea2c
rifatbroh <163645638+rifatbroh@users.noreply.github.com>
2024-12-14 21:51:51 +0600

    Add files via upload

---
+++ Weak 7/Day_03/A_Food_for_Animals.cpp
@@ -0,0 +1,57 @@
+/*
+    MD Rifat Hossain
+
+    Institute:- BAIUST
+    Country:- BANGLADESH
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define OLD_MAXTAN                 \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+#define ll long long
+#define ld long double
+#define yes cout << "YES"
+#define no cout << "NO"
+#define pb push_back
+#define vi vector<int>
+#define vp vector<pair<int, int>>
+#define mp map<int, int>
+#define vec(v) v.begin(), v.end()
+/* !    !   !   !   !   !   !   */
+#define PI 3.1415926535897932384626433832795
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+void solve()
+{
+    int a, b, c, x, y;
+    cin >> a >> b >> c >> x >> y;
+
+    if (a+b+c >= x+y && a >= x && b >= y)
+        yes;
+    else if (a+b+c >= x+y && a+c >= x && b+c >= y)
+        yes;
+    else
+        no;
+}
+
+int main()
+{
+    OLD_MAXTAN
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+        cout<<"\n";
+    }
+    return 0;
+}
