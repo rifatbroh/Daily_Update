b4b34c821956eb2eb1e73fcea76e56a81be1458e
rifatbroh <163645638+rifatbroh@users.noreply.github.com>
2024-12-19 20:53:24 +0600

    Add files via upload

---
+++ Weak 7/Day_04/D_Deletive_Editing.cpp
@@ -0,0 +1,67 @@
+/*
+    MD Rifat Hossain
+
+    Institute:- BAIUST
+    Country:- BANGLADESH
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define OLD_MAXTAN                 \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+#define ll long long
+#define ld long double
+#define yes cout << "YES"
+#define no cout << "NO"
+#define pb push_back
+#define vi vector<int>
+#define vp vector<pair<int, int>>
+#define mp map<char, int>
+#define vec(v) v.begin(), v.end()
+/* !    !   !   !   !   !   !   */
+#define PI 3.1415926535897932384626433832795
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+void solve()
+{
+    /* _THE^MAXTAN_*/
+    string a, b;
+    cin >> a >> b;
+    mp m;
+    for (auto ch : a) m[ch]++;
+    for (auto ch : b) m[ch]--;
+
+    for (auto [x, y]: m){
+        for (int i=0; i<a.size(); i++){
+            if (a[i] == x && y > 0){
+                a.erase(i, 1);
+                y--;
+                i--;
+            }
+        }
+    }
+    if (a == b)
+        yes;
+    else
+        no;
+}
+int main()
+{
+    OLD_MAXTAN
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+        cout<<"\n";
+    }
+    return 0;
+}
